d9c493fa49fbaa2dece5b1d134c39b1aacf1222d
yurchenko08 <yurchenko08@gmail.com>
2023-06-12 22:24:27 +0300

    logic for nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,8 +2,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
-import { signIn, signOut, useSessions, getProvider } from 'next-auth/react';
+import { signIn, signOut, useSessions, getProviders } from 'next-auth/react';
 function Nav() {
+  const isUserLoggedIn = false;
+  const [providers, setProviders] = useState(null);
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+      setProviders();
+    };
+  }, []);
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
@@ -14,7 +23,44 @@ function Nav() {
           height={30}
           className='object-contain'
         />
+        <p className='logo_text'>MindShare</p>
       </Link>
+      {/*Mobile navigation*/}
+      <div className='sm:flex hidden'>
+        {isUserLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-mind' className='black_btn'>
+              Create new post
+            </Link>
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+            <Link href='/profile'>
+              <Image
+                src='/assets/images/logo.svg'
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='Profile'
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 }
