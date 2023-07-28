5853509fc9752df32f0ed062c6c345361553e792
yurchenko08 <yurchenko08@gmail.com>
2023-07-28 17:18:12 +0300

    add abillity to copy post/clipboard

--- components/MindCard.jsx
+++ components/MindCard.jsx
@@ -6,6 +6,13 @@ import { usePathname, useRouter } from 'next/navigation';
 
 const MindCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState('');
+  const handleCopy = () => {
+    setCopied(post.mind);
+    navigator.clipboard.writeText(post.mind);
+    setTimeout(() => {
+      setCopied('');
+    }, 3000);
+  };
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
@@ -26,7 +33,7 @@ const MindCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             </p>
           </div>
         </div>
-        <div className='copy_btn' onClick={() => {}}>
+        <div className='copy_btn' onClick={handleCopy}>
           <Image
             src={
               copied === post.mind
