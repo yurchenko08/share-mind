a01b898b9dda428f14858d5bb288da5cf0e1c49a
yurchenko08 <yurchenko08@gmail.com>
2023-07-31 20:28:52 +0300

    logic for mindCard

--- components/MindCard.jsx
+++ components/MindCard.jsx
@@ -5,6 +5,9 @@ import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
 const MindCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
   const [copied, setCopied] = useState('');
   const handleCopy = () => {
     setCopied(post.mind);
@@ -54,6 +57,22 @@ const MindCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm cursor-pointer green_gradient'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font-inter text-sm cursor-pointer orange_gradient'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
