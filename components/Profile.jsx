153f79548c623fd417e5c523be1c4cf78b677a38
yurchenko08 <yurchenko08@gmail.com>
2023-07-31 20:25:11 +0300

    ui for profile page

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import MindCard from './MindCard';
 
-function Profile() {
-  return <div>Profile</div>;
+function Profile({ name, desc, data, handleEdit, handleDelete }) {
+  return (
+    <section className='w-full '>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{name} Profile</span>
+      </h1>
+      <p className='desc text-left'>{desc}</p>
+      <div className='mt-10 prompt_layout'>
+        {data.map((post) => (
+          <MindCard
+            key={post._id}
+            post={post}
+            handleEdit={() => {
+              handleEdit && handleEdit(post);
+            }}
+            handleDelete={() => {
+              handleDelete && handleDelete(post);
+            }}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Profile;
