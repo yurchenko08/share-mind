60b98f016846999047f761af21c2f18cb7346d03
yurchenko08 <yurchenko08@gmail.com>
2023-07-31 20:46:03 +0300

    +api routes for get/update single mind

---
+++ app/api/mind/[id]/route.js
@@ -0,0 +1,34 @@
+import { connectToDb } from '@utils/database';
+import Mind from '@models/mind';
+
+//GET
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDb();
+    const mind = await Mind.findById(params.id).populate('creator');
+    if (!mind) return new Response('Mind not found', { status: 404 });
+    return new Response(JSON.stringify(mind), { status: 200 });
+  } catch (error) {
+    new Response('Failed to fetch mind', { status: 500 });
+  }
+};
+
+//PATCH
+export const PATCH = async (req, { params }) => {
+  const { mind, tag } = await req.json();
+  try {
+    await connectToDb();
+    const existingMind = await Mind.findById(params.id);
+    if (!existingMind) {
+      return new Response('Prompt not found', { status: 404 });
+    }
+    existingMind.mind = mind;
+    existingMind.tag = tag;
+    await existingMind.save();
+    return new Response(JSON.stringify(existingMind), { status: 200 });
+  } catch (error) {
+    return new Response('Failed to update mind', { status: 500 });
+  }
+};
+
+//DELETE
