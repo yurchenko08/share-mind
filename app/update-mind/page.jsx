81ff75e595d9bfff45a25db1a6293718a90e632c
yurchenko08 <yurchenko08@gmail.com>
2023-08-01 20:12:45 +0300

    *fix bugs with editing own minds

--- app/update-mind/page.jsx
+++ app/update-mind/page.jsx
@@ -1,56 +1,66 @@
 'use client';
+
 import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+
 import Form from '@components/Form';
-import Mind from '@models/mind';
-const EditMind = () => {
+
+const UpdateMind = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const mindId = searchParams.get('id');
-  const router = useRouter();
-  const [submitting, setSubmitting] = useState(false);
+
   const [post, setPost] = useState({ mind: '', tag: '' });
+  const [submitting, setIsSubmitting] = useState(false);
+
   useEffect(() => {
     const getMindDetails = async () => {
       const response = await fetch(`/api/mind/${mindId}`);
       const data = await response.json();
+
       setPost({
         mind: data.mind,
         tag: data.tag,
       });
     };
+
     if (mindId) getMindDetails();
   }, [mindId]);
-  //   const createMind = async (e) => {
-  //     e.preventDefault();
-  //     setSubmitting(true);
-  //     try {
-  //       const response = await fetch('/api/mind/new', {
-  //         method: 'POST',
-  //         body: JSON.stringify({
-  //           mind: post.mind,
-  //           userId: session?.user.id,
-  //           tag: post.tag,
-  //         }),
-  //       });
-  //       console.log(response);
-  //       if (response.ok) {
-  //         router.push('/');
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       setSubmitting(false);
-  //     }
-  //   };
+
+  const updateMind = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    if (!mindId) return alert('Missing PromptId!');
+
+    try {
+      const response = await fetch(`/api/mind/${mindId}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          mind: post.mind,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <Form
       type='Edit'
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={() => {}}
+      handleSubmit={updateMind}
     />
   );
 };
 
-export default EditMind;
+export default UpdateMind;
